7709d1f19bb4b11f8e31f6e44ec041f2b3a4241e
bayaya-devi <bayaya-devi@users.noreply.github.com>
2026-07-18 18:44:40 +0100

    Make juz speed challenge more dynamic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mawahib-offline-v19-prof-dashboard-surah-fixes-20260718';
+const CACHE_VERSION = 'mawahib-offline-v20-speed-challenge-20260718';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const RUNTIME_CACHE = CACHE_VERSION + '-runtime';
 
